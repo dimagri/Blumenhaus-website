569de2ca04a8a76a48c3a2b3f46c691a8d43604c
dimagri <demon-2008@inbox.ru>
2016-02-09 11:32:04 +0200

    finished all static pages

--- header.php
+++ header.php
@@ -1,16 +1,16 @@
 	<header class="wrap header">
 		<nav class="menu-nav menu-nav-left">
 			<ul>
-				<li><a href="">Каталог</a></li>
-				<li><a href="">Оформление мероприятий</a></li>
+				<li><a href="/blumenhaus/catalog.php">Каталог</a></li>
+				<li><a href="/blumenhaus/events.php">Оформление мероприятий</a></li>
 			</ul>
 		</nav>
 		<nav class="menu-nav menu-nav-right">
 			<ul>
-				<li><a href="">О студии</a></li>
-				<li><a href="">Доставка</a></li>
-				<li><a href="">Контакты</a></li>
+				<li><a href="/blumenhaus/about.php">О студии</a></li>
+				<li><a href="/blumenhaus/delivery.php">Доставка</a></li>
+				<li><a href="/blumenhaus/contacts.php">Контакты</a></li>
 			</ul>
 		</nav>
-		<h1 class="header-logo"><a href="/"><img src="images/logo-header.svg" alt=""></a></h1>
+		<h1 class="header-logo"><a href="/blumenhaus/"><img src="images/logo-header.svg" alt=""></a></h1>
 	</header>
