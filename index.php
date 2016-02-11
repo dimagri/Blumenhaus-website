2488949c45d98e61a5edede27f376826c4de02a8
dimagri <demon-2008@inbox.ru>
2016-02-11 10:53:48 +0200

    created wordpress theme

--- index.php
+++ index.php
@@ -1,133 +1,17 @@
-<!DOCTYPE html >
-<html lang="ru" >
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Index</title>
-<link rel="stylesheet" type="text/css" href="fonts/stylesheet.css" />
-<link rel="stylesheet" type="text/css" href="styles/styles.css" />
-</head>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-<body>
-	<section class="preloader" id="preloader" >
-		<img src="images/preloader-logo.svg" alt="" class="preloader-logo">
-		<p>СТУДИЯ ЦВЕТОВ</p>
-		<span class="preloader-arrow-down" id="preloader-down" ></span>
-	</section>
-	<?php 
-		include 'header.php';
-	?>
-	<main class="content">
-		<section class="slider">
-			<ul>
-				<li>
-					<img src="images/02.jpg" alt="">
-					<div class="flower-info">
-						<h2 class="flower-name">COLORS</h2>
-						<span class="flower-description">Гиацинт, Анемон</span>
-					</div>
-				</li>
-				<li>
-					<img src="images/03.jpg" alt="">
-					<div class="flower-info">
-						<h2 class="flower-name">AFINA</h2>
-						<span class="flower-description">Гиацинт, Анемон</span>
-					</div>
-				</li>
-				<li>
-					<img src="images/04.jpg" alt="">
-					<div class="flower-info">
-						<h2 class="flower-name">AFRODITA</h2>
-						<span class="flower-description">Гиацинт, Анемон</span>
-					</div>
-				</li>
-			</ul>
-		</section>
-		<section class="about-studio-block wrap">
-			<h2 class="section-title">О СТУДИИ</h2>
-			<section class="text">
-				<p>Студия флористики BLUMENHAUS – это команда опытных и талантливых флористов, которые любят цветы и свою работу. Годы опыта позволили нам реализовать огромное количестве работ, провести тысячи творческих экспериментов, изучить все существующие флористические техники и придумать множество своих, чтобы сегодня уверенно сказать: мы знаем о цветах все и можем реализовать оформление цветами на любой вкус и по любому поводу!</p>
-				<p>BLUMENHAUS – это не просто салон цветов в центре Киеве, это оазис цветочного настроения и идей!</p>
-			</section>
-		</section>
-		<section class="catalog-block">
-			<h2 class="section-title">КАТАЛОГ БУКЕТОВ</h2>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-			<ul class="catalogs-list-extended">
-				<li>
-					<div class="inner">
-						<img src="images/41.jpg" alt="">
-						<div class="catalog-info">
-							<h3 class="catalog-name">УНИВЕРСАЛЬНЫЕ</h3>
-							<span class="catalog-category">БУКЕТЫ</span>
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="inner">
-						<img src="images/44.jpg" alt="">
-						<div class="catalog-info">
-							<h3 class="catalog-name">ПРАЗДНИЧНЫЕ</h3>
-							<span class="catalog-category">БУКЕТЫ</span>
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="inner">
-						<img src="images/45.jpg" alt="">
-						<div class="catalog-info">
-							<h3 class="catalog-name">МУЖСКИЕ</h3>
-							<span class="catalog-category">БУКЕТЫ</span>
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="inner">
-						<img src="images/46.jpg" alt="">
-						<div class="catalog-info">
-							<h3 class="catalog-name">ДЕТСКИЕ</h3>
-							<span class="catalog-category">БУКЕТЫ</span>
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="inner">
-						<img src="images/40.jpg" alt="">
-						<div class="catalog-info">
-							<h3 class="catalog-name">СВАДЕБНЫЕ</h3>
-							<span class="catalog-category">БУКЕТЫ</span>
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="inner">
-						<img src="images/47.jpg" alt="">
-						<div class="catalog-info">
-							<h3 class="catalog-name">ДОПОЛНЕНИЕ</h3>
-							<span class="catalog-category">К БУКЕТАМ</span>
-						</div>
-					</div>
-				</li>
-			</ul>
-		</section>
-		<section class="instagram-block">
-			<h2 class="section-title">INSTAGRAM</h2>
-		</section>
-	</main>
-	<footer class="footer wrap">
-		<nav class="menu-nav menu-nav-left">
-			<ul>
-				<li><a href="">Каталог</a></li>
-				<li><a href="">Оформление мероприятий</a></li>
-			</ul>
-		</nav>
-		<nav class="menu-nav menu-nav-right">
-			<ul>
-				<li><a href="">О студии</a></li>
-				<li><a href="">Доставка</a></li>
-				<li><a href="">Контакты</a></li>
-			</ul>
-		</nav>
-		<h1 class="footer-logo"><a href="/"><img src="images/footer-logo.svg" alt=""></a></h1>
-	</footer>
-</body>
-</html>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
